94547b5b926182d90264c96f8d34c9580cd8e250
tsabitasyaa <tsasya16@gmail.com>
2025-03-20 00:06:45 +0700

    Add files via upload

---
+++ Praktikum4/BangunDatar.java
@@ -0,0 +1,51 @@
+public class BangunDatar {
+    private int jmlSisi;
+    private String warna;
+    private String border;
+    private static int counterBangunDatar = 0;
+
+    public BangunDatar() {
+        counterBangunDatar++;
+    }
+
+    public BangunDatar(int jmlSisi, String warna, String border) {
+        this.jmlSisi = jmlSisi;
+        this.warna = warna;
+        this.border = border;
+        counterBangunDatar++;
+    }
+
+    public static void printCounterBangunDatar() {
+        System.out.println("Jumlah objek bangun datar: " + counterBangunDatar);
+    }
+
+    public int getJmlSisi() {
+        return jmlSisi;
+    }
+
+    public void setJmlSisi(int jmlSisi) {
+        this.jmlSisi = jmlSisi;
+    }
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+
+    public String getBorder() {
+        return border;
+    }
+
+    public void setBorder(String border) {
+        this.border = border;
+    }
+
+    public void printInfo() {
+        System.out.println("Jumlah sisi: " + jmlSisi);
+        System.out.println("Warna: " + warna);
+        System.out.println("Border: " + border);
+    }
+}
