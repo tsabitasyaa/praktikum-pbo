4fd01cdbe1acc3532d99e74d24f4b7a8679c9f5a
tsabitasyaa <tsasya16@gmail.com>
2025-04-28 00:02:40 +0700

    Update TestPolimorfisme.java

--- praktikum6/TestPolimorfisme.java
+++ praktikum6/TestPolimorfisme.java
@@ -19,3 +19,17 @@ public class TestPolimorfisme {
         }
     }
 }
+
+/* 
+2. Jelaskan manfaat polimorfisme pada kasus ini.
+=>  - Menyederhanakan kode: bisa menyimpan semua objek subclass (Programmer, Manajer) dalam satu ArrayList<Pegawai>.
+    - Memudahkan perulangan: Tanpa perlu tahu tipe pastinya, cukup pakai emp.tampilData(); di dalam loop.
+    - Fleksibilitas tinggi: Kalau nanti ada subclass baru misal Designer, kita tinggal buat objeknya dan tambahkan ke list tanpa ubah kode utama.
+    - Maintainability lebih baik: Program jadi lebih mudah diperluas (extensible) dan dirawat.\
+
+3. Apabila pada main program perlu menambahkan pegawai4 dan pegawai5! Apa
+permasalahan yang muncul jika diterapkan tanpa polimorfisme (inclusion)?
+=>  - Tidak bisa menyimpan semua objek (Programmer, Manajer) dalam satu ArrayList<Pegawai>.
+    - Harus membuat list terpisah atau variable terpisah untuk setiap tipe (Programmer, Manajer, dll.).
+    - Setiap penambahan objek baru (seperti pegawai4, pegawai5) akan memerlukan perubahan besar di kode.
+*/
