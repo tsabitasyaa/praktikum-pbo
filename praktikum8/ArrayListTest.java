53ce80a47e60c44d31b2fee8009cbf4a1f79021a
tsabitasyaa <tsasya16@gmail.com>
2025-05-14 20:29:02 +0700

    Create ArrayListTest.java

---
+++ praktikum8/ArrayListTest.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package praktikum7;
+
+/**
+ *
+ * @author tsabi
+ */
+
+import java.util.ArrayList;
+
+public class ArrayListTest {
+    public static void main(String[] args){
+        ArrayList<String> strings = new ArrayList<String>();
+        strings.add("praktikum");
+        strings.add("collection");
+        strings.add("dan generics");
+        strings.remove("praktikum");
+        for(String s : strings){
+            System.out.print(s+" ");
+        }
+    }
+}/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package praktikum7;
+
+/**
+ *
+ * @author tsabi
+ */
+
+import java.util.ArrayList;
+
+public class ArrayListTest {
+    public static void main(String[] args){
+        ArrayList<String> strings = new ArrayList<String>();
+        strings.add("praktikum");
+        strings.add("collection");
+        strings.add("dan generics");
+        strings.remove("praktikum");
+        for(String s : strings){
+            System.out.print(s+" ");
+        }
+    }
+}
