9f11d8656497af099a4c829d56a8bbc4e769983c
tsabitasyaa <tsasya16@gmail.com>
2025-05-14 20:32:24 +0700

    Update ArrayListTest.java

--- praktikum8/ArrayListTest.java
+++ praktikum8/ArrayListTest.java
@@ -13,35 +13,15 @@ import java.util.ArrayList;
 
 public class ArrayListTest {
     public static void main(String[] args){
+        // inisialisasi ArrayList yang hanya dapat berisi objek String
         ArrayList<String> strings = new ArrayList<String>();
+        // menambah elemen
         strings.add("praktikum");
         strings.add("collection");
         strings.add("dan generics");
+        // menghapus elemen
         strings.remove("praktikum");
-        for(String s : strings){
-            System.out.print(s+" ");
-        }
-    }
-}/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package praktikum7;
-
-/**
- *
- * @author tsabi
- */
-
-import java.util.ArrayList;
-
-public class ArrayListTest {
-    public static void main(String[] args){
-        ArrayList<String> strings = new ArrayList<String>();
-        strings.add("praktikum");
-        strings.add("collection");
-        strings.add("dan generics");
-        strings.remove("praktikum");
+        // iterasi pada keseluruhan ArrayList
         for(String s : strings){
             System.out.print(s+" ");
         }
