7695f99833553b0e2b62b544e0fc03e515f6d8ef
tsabitasyaa <tsasya16@gmail.com>
2025-06-20 20:31:03 +0700

    Create DiskonLambda.java

---
+++ praktikum11/DiskonLambda.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package praktikum7;
+
+/**
+ *
+ * @author tsabi
+ */
+
+interface IDiskon{
+    public double hitungDiskon(int harga);
+}
+
+public class DiskonLambda {
+    public static void main(String[] args){
+        IDiskon diskonMerdeka = new IDiskon(){
+            public double hitungDiskon(int harga){
+                return harga - (harga * 0.3);
+            }
+        };
+        IDiskon diskonLebaran = (harga) -> harga - (harga * 0.4);
+        
+        IDiskon diskonBiasa = (harga) -> {
+            return harga - (harga * 0.1);
+        };
+        System.out.println("Diskon Merdeka: "+diskonMerdeka.hitungDiskon(45000));
+        System.out.println("Diskon Lebaran: "+diskonLebaran.hitungDiskon(45000));
+        System.out.println("Diskon biasa: "+diskonBiasa.hitungDiskon(45000));
+    }
+}
